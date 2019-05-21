457e09f0d16b16dc5282556ad357cf678cf6d05e
1303575952@qq.com <1303575952@qq.com>
2019-05-21 15:49:20 +0800

    贪心算法

---
+++ src/chapter8_greedy_algorithms/SetCovering.java
@@ -0,0 +1,41 @@
+package chapter8_greedy_algorithms;
+
+import java.util.*;
+
+import static java.util.Arrays.*;
+
+public class SetCovering {
+
+    public static void main(String[] args) {
+        Set<String> statesNeeded = new HashSet(asList("mt", "wa", "or", "id", "nv", "ut", "ca", "az"));
+        Map<String, Set<String>> stations = new LinkedHashMap<>();
+
+        stations.put("kone", new HashSet<>(asList("id", "nv", "ut")));
+        stations.put("ktwo", new HashSet<>(asList("wa", "id", "mt")));
+        stations.put("kthree", new HashSet<>(asList("or", "nv", "ca")));
+        stations.put("kfour", new HashSet<>(asList("nv", "ut")));
+        stations.put("kfive", new HashSet<>(asList("ca", "az")));
+
+        Set<String> finalStations = new HashSet<String>();
+        while (!statesNeeded.isEmpty()) {
+            String bestStation = null;
+            Set<String> statesCovered = new HashSet<>();
+
+            for (Map.Entry<String, Set<String>> station : stations.entrySet()) {
+                Set<String> covered = new HashSet<>(statesNeeded);
+                covered.retainAll(station.getValue());
+
+                if (covered.size() > statesCovered.size()) {
+                    bestStation = station.getKey();
+                    statesCovered = covered;
+                }
+                statesNeeded.removeIf(statesCovered::contains);
+
+                if (bestStation != null) {
+                    finalStations.add(bestStation);
+                }
+            }
+        }
+        System.out.println(finalStations); // [ktwo, kone, kthree, kfive]
+    }
+}
